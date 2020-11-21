10fe3148756fdfb3dbb864a3d22a927c0d2a9a6a
Gagne, Martin <martin.gagne@oracle.com>
2020-11-20 22:44:26 -0500

    Class base methods working

--- memdb.py
+++ memdb.py
@@ -1,17 +1,49 @@
-####################################################################
-#
-# Memdb - A simple python exercise of in memory DBMS simulation
-#
-# Author : Martin Gagne
-#
-# Email : martin.gagneqc@gmail.com
-#
-####################################################################
 
+class memDb:
+    # Class memDb is storing values in a dictionary with support for transactions and commits
 
-def main():
-    print("It works")
+    DataDict = dict()
 
+    def put(key, value, transactionId=0):
+        try:
+            memDb.DataDict[key] = value
+            return
+        except:
+            print("An error occured")
+        
 
-if __name__ == "__main__":
-    main()
+    def get(key, transactionId=0):
+        try:
+            return memDb.DataDict[key]
+        except:
+            print("An error occured")
+
+        
+
+    def delete(key, transactionId=0):
+        try:
+            del memDb.DataDict[key]
+            return
+        except:
+            print("An error occured")
+
+    def createTransaction(transactionId):
+        try:
+            #code here
+            return
+        except:
+            print("An error occured while executing ")
+
+    def rollbackTransaction(transactionId):
+        try:
+            #code here
+            return
+        except:
+            print("An error occured")
+
+    def commitTransaction(transactionId):
+        try:
+            #code here
+            return
+        except:
+            print("An error occured")
---
+++ testRoutine.py
@@ -0,0 +1,29 @@
+####################################################################
+#
+# Memdb - A simple python exercise of in memory DBMS simulation
+#
+# Author : Martin Gagne
+#
+# Email : martin.gagneqc@gmail.com
+#
+####################################################################
+
+from memDb import memDb
+
+
+def main():
+    myDb = memDb
+
+    myDb.put("example", "foo") 
+
+    myDb.get("example")
+
+    myDb.delete("example") 
+
+    myDb.get("example") 
+
+    myDb.delete("example") 
+
+
+if __name__ == "__main__":
+    main()
