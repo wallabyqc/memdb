b633a228e389ba4f74201d462d4dc854fd52ec0e
Gagne, Martin <martin.gagne@oracle.com>
2020-11-20 21:20:24 -0500

    Set the table

---
+++ memdb.py
@@ -0,0 +1,17 @@
+####################################################################
+#
+# Memdb - A simple python exercise of in memory DBMS simulation
+#
+# Author : Martin Gagne
+#
+# Email : martin.gagneqc@gmail.com
+#
+####################################################################
+
+
+def main():
+    print("It works")
+
+
+if __name__ == "__main__":
+    main()
