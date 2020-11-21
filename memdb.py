08db5056cda2b93217a9c06e47da566cc82bf9dd
Gagne, Martin <martin.gagne@oracle.com>
2020-11-21 01:08:35 -0500

    Fully working class and routine

--- memdb.py
+++ memdb.py
@@ -1,49 +1,137 @@
+########################################################################################################################################
+#
+# Class memDb, python exercise to simulate a in memory DBMS database system with transaction/commit/rollback feature
+#
+# Author : Martin Gagne
+#
+# Email : martin.gagneqc@gmail.com
+#
+# Note : More verbose comments is intended to explain the code for the purpose of the technical exercise
+#
+########################################################################################################################################
+
+from datetime import datetime
+
 
 class memDb:
-    # Class memDb is storing values in a dictionary with support for transactions and commits
+    # Class memDb initialization
 
-    DataDict = dict()
+    dataDict = dict()
+    transactionMaster = dict()
 
-    def put(key, value, transactionId=0):
+    def put(key, value, transactionId=None):
         try:
-            memDb.DataDict[key] = value
+            if transactionId != None:
+
+                if transactionId in memDb.transactionMaster:
+                    transactionHeader = "trx" + str(transactionId) + ";"
+                    transactionKey = transactionHeader + str(key)
+                    memDb.dataDict[transactionKey] = [value, datetime.now()]
+                else:
+                    raise Exception("This transaction doesn't exist")
+
+            else:
+                memDb.dataDict[key] = [value, datetime.now()]
             return
-        except:
-            print("An error occured")
-        
 
-    def get(key, transactionId=0):
+        except Exception as error:
+            print("An error occured while affecting the data record: " + str(error))
+
+    def get(key, transactionId=None):
         try:
-            return memDb.DataDict[key]
-        except:
-            print("An error occured")
+            if transactionId != None:
+                if transactionId in memDb.transactionMaster:
+                    transactionHeader = "trx" + str(transactionId) + ";"
+                    transactionKey = transactionHeader + str(key)
+                    return memDb.dataDict[transactionKey][0]
+                else:
+                    raise Exception("This transaction doesn't exist")
 
-        
+            return memDb.dataDict[key][0]
 
-    def delete(key, transactionId=0):
+        except Exception as error:
+            print("An error occured while retrieving the data record: " + str(error))
+
+    def delete(key, transactionId=None):
         try:
-            del memDb.DataDict[key]
+            if transactionId != None:
+                if transactionId in memDb.transactionMaster:
+                    transactionHeader = "trx" + str(transactionId) + ";"
+                    transactionKey = transactionHeader + str(key)
+                    del memDb.dataDict[transactionKey]
+                else:
+                    raise Exception("This transaction doesn't exist")
+
+            del memDb.dataDict[key]
             return
-        except:
-            print("An error occured")
+        except Exception as error:
+            print("An error occured while deleting the data record: " + str(error))
 
     def createTransaction(transactionId):
         try:
-            #code here
-            return
-        except:
-            print("An error occured while executing ")
+            if transactionId in memDb.transactionMaster:
+                raise Exception("This transaction id is already being used")
+            memDb.transactionMaster[transactionId] = datetime.now()
+
+            return None
+            
+        except Exception as error:
+            print("An error occured while creating the transaction: " + str(error))
 
     def rollbackTransaction(transactionId):
+
+        transactionHeader = "trx" + str(transactionId) + ";"
+
         try:
-            #code here
-            return
-        except:
-            print("An error occured")
+
+            if transactionId in memDb.transactionMaster:
+
+                for key in list(memDb.dataDict.keys()):
+
+                    if transactionHeader in str(key):
+                        del memDb.dataDict[key]
+
+                del memDb.transactionMaster[transactionId]
+
+            else:
+
+                raise Exception("This transaction doesn't exist")
+
+            return None
+
+        except Exception as error:
+            print("An error occured while rolling back the transaction: " + str(error))
 
     def commitTransaction(transactionId):
+
+        transactionHeader = "trx" + str(transactionId) + ";"
+
         try:
-            #code here
-            return
-        except:
-            print("An error occured")
+            if transactionId in memDb.transactionMaster:
+
+                # Loop all transaction related records and check if current stored data was altered after transaction was started.
+
+                for key in list(memDb.dataDict.keys()):
+                    if transactionHeader in str(key):
+                        originalKey = str(key).replace(transactionHeader, '')
+                        if originalKey in memDb.dataDict:
+                            if memDb.dataDict[originalKey][1] > memDb.transactionMaster[transactionId]:
+                                raise Exception(
+                                    "The transaction commit failed because one or more records was altered after transaction was initiated")
+
+                # Loop all transaction related records and update stored data from transaction records
+
+                for key in list(memDb.dataDict.keys()):
+                    if transactionHeader in str(key):
+                        originalKey = str(key).replace(transactionHeader, '')
+                        memDb.dataDict[originalKey] = [
+                            memDb.dataDict[key][0], datetime.now()]
+                        del memDb.dataDict[key]
+                del memDb.transactionMaster[transactionId]
+            else:
+                raise Exception(
+                    "The transaction commit failed because the transaction doesn't exist")
+            return None
+
+        except Exception as error:
+            print("An error occured while commiting transaction: " + str(error))
--- testRoutine.py
+++ testRoutine.py
@@ -1,12 +1,12 @@
-####################################################################
+########################################################################################################################################
 #
-# Memdb - A simple python exercise of in memory DBMS simulation
+# testRoutine.py - A simple python routine to tests cases of memDb class
 #
 # Author : Martin Gagne
 #
 # Email : martin.gagneqc@gmail.com
 #
-####################################################################
+########################################################################################################################################
 
 from memDb import memDb
 
@@ -14,15 +14,79 @@ from memDb import memDb
 def main():
     myDb = memDb
 
+
+    #Here is an example request sequence without transactions: 
+
     myDb.put("example", "foo") 
 
-    myDb.get("example")
+    myDb.get("example") # returns “foo” 
 
     myDb.delete("example") 
 
-    myDb.get("example")
+    myDb.get("example") #returns null 
 
     myDb.delete("example") 
+
+
+    #Here is an example request sequence: 
+
+ 
+    myDb.createTransaction("abc")
+
+    myDb.put("a", "foo", "abc")
+
+    myDb.get("a", "abc")# returns "foo" 
+
+    myDb.get("a")# returns null 
+
+    
+
+    myDb.createTransaction("xyz")
+
+    myDb.put("a", "bar", "xyz")
+
+    myDb.get("a", "xyz")# returns "bar" 
+
+    myDb.commitTransaction("xyz")
+
+    myDb.get("a")# returns "bar" 
+
+    
+
+    myDb.commitTransaction("abc")# failure 
+
+    
+
+    myDb.get("a")# returns "bar" 
+
+    
+
+    myDb.createTransaction("abc")
+
+    myDb.put("a", "foo", "abc")
+
+    myDb.get("a")# returns "bar" 
+
+    myDb.rollbackTransaction("abc")
+
+    myDb.put("a", "foo", "abc")# failure 
+
+    myDb.get("a")# returns "bar" 
+
+    
+
+    myDb.createTransaction("def")
+
+    myDb.put("b", "foo", "def")
+
+    myDb.get("a", "def")# returns "bar" 
+
+    myDb.get("b", "def")# returns "foo" 
+
+    myDb.rollbackTransaction("def")
+
+    myDb.get("b")# returns null 
+
 
 
 if __name__ == "__main__":
